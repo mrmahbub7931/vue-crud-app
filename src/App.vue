06cb06b41a79f6306bf88ecc42bf12142e82b3a2
Mahbubar Rahman <md.mahbub7931@gmail.com>
2022-12-01 19:19:32 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+      <h1 class="text-center text-2xl">CRUD APPLICATION</h1>
+      <div class="m-8 bg-sky-100 h-screen">
+          <employee-details v-bind:employees ="employees" @edit="editEmployee"></employee-details>
+      </div>
+</template>
+
+<script>
+  import EmployeeDetails from './components/EmployeeDetails.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      EmployeeDetails
+    },
+    data(){
+        return {
+            employees: []
+        }
+    },
+    methods: {
+        async getEmployees(){
+            try {
+                const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+                const data = await response.json()
+                this.employees = data
+            } catch (error) {
+                console.error('Error occured while retrieving employees: ' +error);
+            }
+        },
+
+        async editEmployee() {
+            
+        }
+    },
+    mounted() {
+        this.getEmployees()
+    }
+  }
+</script>
---
+++ src/components/EmployeeDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <p class="text-black m-5">Employee Details page</p>
+    <p v-if="employees.length < 1">No records Found!</p>
+    <div class="overflow-x-auto" v-else>
+    <table class="table w-full">
+        <!-- head -->
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!-- row 1 -->
+        <tr v-for="employee in employees" v-bind:key="employee.id">
+            <th>{{employee.id}}</th>
+            <td>{{employee.name}}</td>
+            <td>{{employee.email}}</td>
+            <td>
+                <button class="btn btn-warning ">Edit</button>
+                <button class="btn btn-error ml-4">Delete</button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+<script>
+    export default {
+        name: 'employee-details',
+        props: {
+            employees: Array
+        }
+    }
+</script>
